45811d26afdd4a6d1ce213c9485cb69b6e19fef4
wcg_windows <wcg14231022@gmail.com>
2022-09-04 12:55:02 +0800

    new commit

---
+++ js_day_01/click.js
@@ -0,0 +1,13 @@
+function mousestatus(e){
+    if (!e) e = window.Event;
+    btn = e.button;
+    whichone = (btn < 2) ? "Left" : "Right";
+    message = e.type + " : " + whichone + "\n";
+    document.form1.info.value += message;
+}
+
+obj = document.getElementById("testlink");
+obj.onmousedown = mousestatus;
+obj.onmouseup = mousestatus;
+obj.onclick = mousestatus;
+obj.ondblclick = mousestatus;
